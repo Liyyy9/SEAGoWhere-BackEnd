9debe135bc6f1001e9cc8ba9e93dea5025ceb290
Rushifx <tohbingshan.colin@gmail.com>
2024-10-17 11:59:05 +0800

    noticeable errors fixed

--- src/main/java/com/example/seagowhere/Model/User.java
+++ src/main/java/com/example/seagowhere/Model/User.java
@@ -58,4 +58,28 @@ public class User implements UserDetails {
         return List.of(new SimpleGrantedAuthority(this.role.name()));
     }
 
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
